1b77705140b78299e2e3d27267b91529d6a56ac2
Virginia Rodriguez <virginia88@gmail.com>
2014-11-15 18:39:05 -0200

    Agregar scraper asistencias resumen

--- scrapers/asistencias.py
+++ scrapers/asistencias.py
@@ -29,7 +29,7 @@ def scrape(options):
   fecha_fin = options.get('fechafin', hoy)
 
   query = '?Cuerpo=%s&Legistaltura=%s' % (cuerpo, NRO_LEGISLATURA_ACTUAL)
-  url = base_url + '/gxemule/ConsAsistenciaBrief.asp' + query
+  url = base_url + '/gxemule/ConsAsistencia.asp' + query
 
   logging.info("Escrapeando informacion de asistencias desde la pagina del parlamento.\nURL: %s.\n" % url)
 
@@ -46,7 +46,9 @@ def scrape(options):
   	'Ini'         : fecha_inicio,
   	'Fin'         : fecha_fin,
   	'Legislatura' : NRO_LEGISLATURA_ACTUAL,
-  	'Fechas'      : 'Seleccionado'
+  	'Fechas'      : 'Seleccionado',
+    'Asistencia'  : 'Legislador',
+    'Orden'       : 'ASC'
   }
 
   # Ejecutar POST con cookie y parametros
@@ -58,89 +60,35 @@ def scrape(options):
   the_page = response.read()
 
   # Guardar pagina
-  filename = "cache/asistencias_por_sesion/camara_%s_%s-%s.html" % (cuerpo, fecha_inicio, fecha_fin)
+  filename = "cache/asistencias/camara_%s_%s-%s.html" % (cuerpo, fecha_inicio, fecha_fin)
   utils.write(the_page, filename)
   doc = lxml.html.document_fromstring(the_page)
 
-  info_sesiones = doc.xpath('//center/a/b')
-  info_asistencias = doc.xpath('//div[@align="justify"]')
+  tabla_datos = doc.xpath('//table[@bordercolordark="#D1D6D5"]')[0]
+  rows = tabla_datos.xpath('tr')
 
-  index = 0
-  sesiones = []
+  info_asistencias = []
 
-  while index < len(info_sesiones):
-    data = {}
+  first = True
+  for row in rows:
+    if first:
+      first = False
+    else:
+      data = {}
+      columnas = row.xpath('td')
+      data['nombre'] = columnas[0].text_content()
+      data['citaciones'] = columnas[1].text_content().strip()
+      data['asistencias'] = columnas[2].text_content()
+      data['faltas_con_aviso'] = columnas[4].text_content()
+      data['faltas_sin_aviso'] = columnas[6].text_content()
+      data['no_citado_por_licencia'] = columnas[8].text_content()
+      data['no_citado_por_estar_ejerciendo_presidencia'] = columnas[9].text_content()
 
-    info_sesion = info_sesiones[index].xpath('text()')
-    data['nro_sesion'] = re.sub(r'\D', "", info_sesion[0])
-    data['fecha'] = info_sesion[1]
+      info_asistencias.append(data)
 
-    data['asistencias'] = []
-    data['faltas_con_aviso'] = []
-    data['faltas_sin_aviso'] = []
-    data['faltas_por_licencia'] = []
-    data['notas'] = []
-
-    notes_keys = {}
-    scrape_asistencias(data, info_asistencias[index], notes_keys)
-
-    sesiones.append(data)
-    index +=1
 
   output_path = "data/asistencias_%s.json" % cuerpo
   utils.write(
-    json.dumps(sesiones, sort_keys=True, indent=1, encoding='utf-8'),
+    json.dumps(info_asistencias, sort_keys=True, indent=1, encoding='utf-8'),
     output_path
   )
-
-
-def scrape_asistencias(data, div, notes_keys):
-  cleaner = Cleaner(remove_tags=['b'])
-  texts = cleaner.clean_html(div).xpath('text()')
-  for text in texts:
-    text = text.strip()
-    if re.match('Asisten los', text):
-      data['asistencias'] = scrape_nombres(text, notes_keys)
-    elif re.match('Falta(n|) con aviso', text):
-      data['faltas_con_aviso'] = scrape_nombres(text, notes_keys)
-    elif re.match('Falta(n|) sin aviso', text):
-      data['faltas_sin_aviso'] = scrape_nombres(text, notes_keys)
-    elif re.match('No citados por licencia', text):
-      data['faltas_por_licencia'] += scrape_nombres(text, notes_keys)
-    elif re.match('En ejercicio de la Presidencia de la', text):
-      data['faltas_por_licencia'] += scrape_nombres(text, notes_keys)
-    elif len(text) > 1:
-      if re.search(r'\((\d)\)', text):
-        data['notas'].append(add_note(text, notes_keys))
-      else:
-        data['asistencias'] = scrape_nombres(text, notes_keys)
-
-
-def scrape_nombres(text, notes_keys):
-  text_parts = text.split(':')
-  text = text_parts[len(text_parts)-1]
-  names = text.replace(' y ', ', ').split(',')
-  return [clean_name(name, notes_keys) for name in names]
-
-def add_note(text, notes_keys):
-  m = re.search(r'\((\d)\)', text)
-  note_ref = m.group(1)
-
-  note_text = re.sub(r'\(\d\)', '', text)
-  note_text = note_text.strip()
-
-  return {notes_keys[note_ref]: note_text}
-
-
-def clean_name(text, notes_keys):
-  text = text.strip()
-  m = re.search(r'\((\d)\)', text)
-
-  text = re.sub(r'\.$', '', text)
-  text = re.sub(r'\(\d\)$', '', text)
-  text = text.strip()
-
-  if m:
-    notes_keys[m.group(1)] = text
-
-  return text
